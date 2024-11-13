59cb2367801d6499f198d285ffacc6c250d5ab67
Minalnagbhidkar <minal.nagbhidkar@gmail.com>
2024-11-13 13:55:44 +0530

    Create after_install.sh

---
+++ after_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Running after install script...."
+
+# Navigate to the application directory
+cd /home/ubuntu/Stickynote-Upgrade
+
+# Install the dependencies listed in package.json
+npm install
+
+echo "Dependencies installed successfully."
